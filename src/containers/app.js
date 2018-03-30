4a8c4b2d2c3aa49046c01f2322de4170028eb633
YanPanMichael <yanpancsu@icloud.com>
2018-03-30 23:59:42 +0800

    add react scorll bar

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
 import { connect } from 'react-redux';
 import { loadApp } from 'actions/app';
 import styles from './app.css';
